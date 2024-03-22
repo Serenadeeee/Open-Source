69e3d34ebf96195a337e7bad658c1bc3d7cfda83
Serenade <camtuwin432@gmail.com>
2024-03-22 22:45:16 +0700

    update all file

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,33 @@
+@extends('layout')
+@section('content')
+<div class="wrap-categories mb-3">
+    <div class="row">
+        <div class="text-center mb-3">
+            <div class="title-content">
+                <img src="{{url('public/client/img/dot-title-left.png')}}" alt=""> Liên hệ với chũng tôi <img src="{{url('public/client/img/dot-title-right.png')}}" alt="">
+            </div>
+        </div>
+        <div class="wrap-products">
+            <div class="row">
+                <div class="col-md-5">
+                    <p>
+                        <div class="title-footer">DK SHOP</div>
+                        <span>&emsp;&emsp;<i class="mr-3 fas fa-map-marker-alt"></i>Địa chỉ:<a class="row-bottom-2" href="https://www.google.com/maps/place/1+Chu+Lai,+Ho%C3%A0+H%E1%BA%A3i,+Ng%C5%A9+H%C3%A0nh+S%C6%A1n,+%C4%90%C3%A0+N%E1%BA%B5ng+550000,+Vietnam/@15.9914776,108.2548915,17z/data=!3m1!4b1!4m5!3m4!1s0x31421095bf75708b:0x8b04e541aff0bbd6!8m2!3d15.9914776!4d108.2570802"> 01 Chu Lai, P. Hòa Hải, Q. Ngũ Hành Sơn, TP. Đà Nẵng</a></span><br>
+                        <span>&emsp;&emsp;<i class="mr-3 fas fa-phone-alt"></i>Điện thoại:<a class="row-bottom-2" href="tel: 0941 547 945"> 077 620 6278  </a></span><br>
+                        <span>&emsp;&emsp;<i class="mr-3 fas fa-envelope"></i>Email:<a class="row-bottom-2" href="mailto: ducna.21it@vku.udn.vn"> ducna.21it@vku.udn.vn</a></span><br>
+                        <span>&emsp;&emsp;<i class="mr-3 fas fa-envelope"></i>Email:<a class="row-bottom-2" href="mailto: kietnq.21it@vku.udn.vn"> kietnq.21it@vku.udn.vn</a></span>
+                    </p>
+                    <p>
+                        
+                    </p>
+                </div>
+                <div class="col-md-7">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d402.44511429868186!2d108.23740852109808!3d15.980160367519487
+                        !2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31421bd1538be79d%3A0x463742589cf6751b!2sRacing%2043%20Shop!5e0!3m2!1svi!2s!4v1638028440705!5m2!1svi!2s"
+                         width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
